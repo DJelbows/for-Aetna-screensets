ddbe1d398f70e7469a1ae4c201d92c0bb78aa093
alejandromcsd <alejandromcsd@gmail.com>
2019-09-20 10:58:04 +1000

    Added test comment

--- src/app.js
+++ src/app.js
@@ -2,8 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import ProfileManager from './components/ProfileManager'
 import 'normalize.css/normalize.css'
-import './styles/styles.scss' 
+import './styles/styles.scss'
 
+// Test comment here
 //ReactDOM.render(<Navigation />, document.getElementById('nav'));
 //ReactDOM.render(<Profile />, document.getElementById('container'));
 ReactDOM.render(<ProfileManager />, document.getElementById('app'));
